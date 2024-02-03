cb4f44790dbc415e21b7e0b9c4b04d0a6af778fb
Ramazan Korucu <rmznkrc11@gmail.com>
2024-02-03 16:51:39 -0500

    Dicee

--- LasVagasDicee/ViewController.swift
+++ LasVagasDicee/ViewController.swift
@@ -9,10 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var image2fField: UIImageView!
+    @IBOutlet weak var image1Field: UIImageView!
+    
+    let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    
+    @IBAction func RollAction(_ sender: Any) {
+        
+        image1Field.image = diceArray[Int.random(in: 0...5)]
+        image2fField.image = diceArray[Int.random(in: 0...5)]
     }
+   
 
 
 }
